6a2bdb408017bf6b255c797bbbd1586479ace170
John Prince <jtprince@gmail.com>
2009-10-01 09:49:24 -0600

    can parse IPI headers properly

---
+++ lib/ms/fasta/ipi/dat.rb
@@ -0,0 +1,10 @@
+
+module Ms
+ module Fasta
+   module Ipi
+     # reader for the IPI .dat.gz file with the .fasta.gz file 
+     class Dat
+     end
+   end
+ end
+end
---
+++ spec/ms/fasta/ipi/dat_spec.rb
@@ -0,0 +1,7 @@
+require File.dirname(__FILE__) + '/../../../tap_test_helper'
+
+require 'ms/fasta/ipi/dat'
+
+class IpiDatSpec < MiniTest::Spec
+  
+end
