38e566706b6fec8e56beacecb9cf605acc74f0b3
John Prince <jtprince@gmail.com>
2009-10-08 12:50:23 -0600

    progress

---
+++ lib/ms/fasta/ipi/dat.rb
@@ -0,0 +1,14 @@
+
+module Ms
+  module Fasta
+    module Ipi
+
+      # This is to create readers to the .dat.gz format that can be downloaded
+      # beside the .fasta.gz file.
+      class Dat
+        def initialize(file)
+        end
+      end
+    end
+  end
+end
